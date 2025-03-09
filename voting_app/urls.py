ed17340fba267678c285ca990fc1b4b1d52da0e2
Vaisakh2004 <vaisakhramakrishnan@gmail.com>
2025-03-09 12:33:51 +0530

    Update urls.py

--- voting_app/urls.py
+++ voting_app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from .views import check_face_data
 
 urlpatterns = [
-    path('register/', views.register_voter, name='register_voter'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('vote/', views.vote, name='vote'),
-    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
-    path('check-face-data/', check_face_data, name='check_face_data'),
+    path('register-voter/', views.register_voter, name='register_voter'),
+    path('register/', views.register_page, name='register_page'),
 ]
